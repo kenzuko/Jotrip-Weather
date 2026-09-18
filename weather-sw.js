b0d24f644f7c44d97e893e8b42833cda06114979
kenzuko <kenzuko@gmail.com>
2026-09-18 17:55:02 +0700

    deploy(weather): bump cache for rain observation upgrade

--- weather-sw.js
+++ weather-sw.js
@@ -1,10 +1,10 @@
-const VERSION="weather-intelligence-2026.09.18.prod10";
+const VERSION="weather-intelligence-2026.09.18.prod11";
 const CACHE=`${VERSION}-static`;
 const STATIC=[
   "/",
   "/index.html",
-  "/weather-v2.css?v=20260918-prod10",
-  "/weather-v2.js?v=20260918-prod10",
+  "/weather-v2.css?v=20260918-prod11",
+  "/weather-v2.js?v=20260918-prod11",
   "/weather-brand.svg",
   "/weather-app-icon.svg",
   "/favicon-64.png",

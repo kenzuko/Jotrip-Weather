f58fcc14731af30644cab9fe4d911f1eef5c5992
kenzuko <kenzuko@gmail.com>
2026-09-20 10:46:15 +0700

    release(weather-v2): rotate cache for probability labels

--- weather-sw.js
+++ weather-sw.js
@@ -1,10 +1,10 @@
-const VERSION="weather-intelligence-2026.09.20.prod17";
+const VERSION="weather-intelligence-2026.09.20.prod18";
 const CACHE=`${VERSION}-static`;
 const STATIC=[
   "/",
   "/index.html",
-  "/weather-v2.css?v=20260920-prod17",
-  "/weather-v2.js?v=20260920-prod17",
+  "/weather-v2.css?v=20260920-prod18",
+  "/weather-v2.js?v=20260920-prod18",
   "/weather-brand.svg",
   "/weather-app-icon.svg",
   "/favicon-64.png",

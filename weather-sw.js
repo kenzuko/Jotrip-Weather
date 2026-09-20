1c61d867b160802d8eeaf2b33ddc6c2decf4e71e
kenzuko <kenzuko@gmail.com>
2026-09-20 11:53:15 +0700

    release(weather-v2): rotate cache for performance build

--- weather-sw.js
+++ weather-sw.js
@@ -1,10 +1,10 @@
-const VERSION="weather-intelligence-2026.09.20.prod19";
+const VERSION="weather-intelligence-2026.09.20.prod20";
 const CACHE=`${VERSION}-static`;
 const STATIC=[
   "/",
   "/index.html",
-  "/weather-v2.css?v=20260920-prod19",
-  "/weather-v2.js?v=20260920-prod19",
+  "/weather-v2.css?v=20260920-prod20",
+  "/weather-v2.js?v=20260920-prod20",
   "/weather-brand.svg",
   "/weather-app-icon.svg",
   "/favicon-64.png",

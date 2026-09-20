bb1a73ed2b77bd6ca83ae2a6c63af0513f435a0a
kenzuko <kenzuko@gmail.com>
2026-09-20 13:33:03 +0700

    release(weather-v2): rotate cache to prod25

--- weather-sw.js
+++ weather-sw.js
@@ -1,10 +1,10 @@
-const VERSION="weather-intelligence-2026.09.20.prod24";
+const VERSION="weather-intelligence-2026.09.20.prod25";
 const CACHE=`${VERSION}-static`;
 const STATIC=[
   "/",
   "/index.html",
-  "/weather-v2.css?v=20260920-prod24",
-  "/weather-v2.js?v=20260920-prod24",
+  "/weather-v2.css?v=20260920-prod25",
+  "/weather-v2.js?v=20260920-prod25",
   "/weather-brand.svg",
   "/weather-app-icon.svg",
   "/favicon-64.png",
